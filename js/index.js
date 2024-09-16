94ff4e68703a21b25415ef2192dc25c6ccb08557
Tymofii-Oleksiuk <jji243372@gmail.com>
2024-09-16 17:07:32 +0200

    Hw2_JS

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const celsius = 30;
+const fahrenheit = 0;
+const x = 18;
+const y = 32;
+console.log(celsius * x / 10 + y + "F°");
+
+// Task 2
+
+const april = 30;
+const hour = 24;
+const minute = 60;
+console.log(april * hour + ' hour');
+console.log(april * minute + ' minute');
+
+// Task 3
+
+const hp = 100;
+const atack = 20;
+console.log(hp - atack);
+
+const price = 100;
+
+const discount = 10;
+
+console.log(price - discount)
