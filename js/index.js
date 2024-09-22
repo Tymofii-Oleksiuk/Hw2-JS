bb47116ee0ec5b3f4339233ae0024943a784a874
Tymofii-Oleksiuk <jji243372@gmail.com>
2024-09-22 14:09:56 +0200

    fixed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 const celsius = 30;
-const fahrenheit = 0;
 const x = 18;
 const y = 32;
-console.log(celsius * x / 10 + y + "F°");
+const fahrenheit = (celsius * x) / 10 + y + "F°";
+
+console.log(fahrenheit);
 
 // Task 2
 
 const april = 30;
 const hour = 24;
 const minute = 60;
-console.log(april * hour + ' hour');
-console.log(april * minute + ' minute');
+console.log(april * hour + " hour");
+console.log(april * minute + " minute");
 
 // Task 3
 
@@ -18,8 +19,38 @@ const hp = 100;
 const atack = 20;
 console.log(hp - atack);
 
+// Task 4 
+
 const price = 100;
+const discount = 100 * 0.10;                                                 
+console.log(price - discount);
 
-const discount = 10;
+// Task 5
 
-console.log(price - discount)
+const numb = 2.77;
+
+console.log(Math.floor(numb));
+
+// Task 6
+
+console.log(Number.parseFloat('2.3434kjh67834'));
+
+// Task 7
+
+console.log(Number.parseInt('21.56gtr'));
+
+// Task 8
+
+const squareNumber = 25;
+
+console.log(Math.sqrt(squareNumber));
+
+// Task 9
+
+const newParseInt = "52";
+
+const newText = 98;
+
+console.log(toString(newText));
+
+console.log(parseInt(newParseInt));
